66b2aa517ac6caf04f606997bfa030dbd2fa1a2a
Eli <eliwaalkes1@gmail.com>
2017-04-03 14:17:22 -0400

    update

--- tree23main.cu
+++ tree23main.cu
@@ -18,29 +18,38 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 	}
 }
 
-__global__ void gpuInsert( int *t, int *insert, int start, int end, int insertPerBlock ){
+__global__ void gpuInsert( int *t, int *insert, int insertPerBlock ){
 
-  //__shared__ int startIndices[gridDim.x];
-  // __shared__ int endIndices[gridDim.x];
+  __shared__ int insertNode[64];
 
   int tid = threadIdx.x;
   int bid = blockIdx.x;
   int root = getChild0(t, 0);
   insert = insert + (insertPerBlock*bid);
-  /*
+
+
   if( tid < insertPerBlock ){
 	int spot = findSpot_arr_d(t, root, insert[tid]);
-	if(t[getKey0(t, spot)] < insert[tid]){
-	  //start[tid] = getChild0(t, spot);
-	  //end[tid] = getChild1(t, spot);}
-	else{
-	  //start[tid] = getChild0(t, spot);
-	  //end[tid] = getChild1(t, spot);
-	}
+	insertNode[tid] = spot;
   }
+
+  __syncthreads();
+
+  /*
+  if(tid == insertPerBlock + 1){
+	int s1 = 0;
+	int f1 = 1;
+	int count = 0;
+	while(insertNode[s1] == insertNode[f1]){
+	  s1++;
+	  f1 += 2;
+	  count += 2;
   */
   
-
+  if( tid < insertPerBlock ){
+	insert_help_arr_d(t, insertNode[tid], createNode_arr_d(t, offsetTotal_d, insert[tid]), -2);
+  }
+  
 }
 
 
@@ -54,18 +63,37 @@ __global__ void findSpots_gpu( int *t, int *insert, int *startInd, int *endInd, 
   int root = getChild0(t, 0);
   insert = insert + (insertPerBlock*bid);
   startInd = startInd + (insertPerBlock*bid);
-  endInd = endInd + (insertPerBlock*bid);
-  
+
+  //  int root = getChild0(t, 0);
+  //  insert = insert + (insertPerBlock*bid);
+ 
   if( tid < insertPerBlock ){
 	int spot = findSpot_arr_d(t, root, insert[tid]);
+	startInd[tid] = spot;
+	if(getKey0(t, spot) > insert[tid]) 
+	  startInd[tid] = getChild0(t,spot);
+	else if(getKey0(t, spot) < insert[tid])
+	  startInd[tid] = getChild1(t,spot);
+	else
+	  startInd[tid] = getChild2(t,spot);
+  }
+  /*
+  if( tid < insertPerBlock ){
+	int spot = findSpot_arr_d(t, 0, insert[tid]);
+	printf("tid: %d, val: %d, spot: %d\n", tid, insert[tid], spot);
+	startInd[tid] = getChild0(t,spot);
+	endInd[tid] = spot;
 	if(t[getKey0(t, spot)] < insert[tid]){
 	  startInd[tid] = getChild0(t, spot);
 	  endInd[tid] = getChild1(t, spot);}
+	else if(t[getKey1(t, spot)] < insert[tid]){
+	  startInd[tid] = getChild1(t, spot);
+	  endInd[tid] = getChild2(t, spot);}
 	else{
-	  startInd[tid] = getChild0(t, spot);
-	  endInd[tid] = getChild1(t, spot);
-	}
-  }
+	  int c = getChild2(t, spot);
+	  startInd[tid] = c;
+	  endInd[tid] = getChild0(t, c);
+	  }*/
 
 }
 
@@ -76,7 +104,7 @@ int main(){
     //INSERT SET
   int *insertSet = (int *) malloc(insertSize*sizeof(int));
   for(int i = 0; i < insertSize; i++)
-	insertSet[i] = 2*i+1;
+	insertSet[i] = i+10;
 
   /*
   //CREATE INITAL TREES
@@ -107,8 +135,8 @@ int main(){
   numNodes = 0;
   int *tGPU = (int *) malloc(14*(treeSize+insertSize)*sizeof(int));
   createRoot_arr(tGPU);
-  for(int i = 1; i < treeSize; i++){
-	insert_arr(tGPU, 0, 2*i);
+  for(int i = 0; i < treeSize; i++){
+	insert_arr(tGPU, 0, 10*i);
   }
   //int start = insert_arr_index(tGPU, 0, insertArray[0]);
   //int end = insert_arr_index(tGPU, 0, insertArray[insertSize-1]);
@@ -132,7 +160,7 @@ int main(){
 
 
   int insertPerBlock = treeSize/num_blocks;  
-  //gpuInsert<<< num_blocks, threads_per_block >>>( d_t23, d_insert, start, end, insertPerBlock );
+  //gpuInsert<<< num_blocks, threads_per_block >>>( d_t23, d_insert, insertPerBlock );
   findSpots_gpu<<< num_blocks, threads_per_block >>>( d_t23, d_insert, startInd_d, endInd_d, insertPerBlock);
   
   int *startInd_h = (int *) malloc(insertSize*sizeof(int));
@@ -143,15 +171,27 @@ int main(){
   cudaMemcpy( (void*) startInd_h , (void*) startInd_d, (insertSize)*sizeof(int) , cudaMemcpyDeviceToHost );
 
 
+  //print_tree(tGPU);
+
+  print_tree(tGPU);
+  printf("Insert Set: \n");
+  
+  for(int i =0; i < insertSize; i++)
+	printf("%d, ", insertSet[i]);
+
+  printf("\n");
+
+  
   for(int i =0; i < insertSize; i++)
 	printf("startInd[%d] = %d, endInd[%d] = %d\n", i, startInd_h[i], i, endInd_h[i]);
-  
+ 
   //print_tree(tCPU);
-  //  print_inorder(tCPU,0);
+  print_inorder(t23_out,0);
 
   //printf("\n-----------------------\n");
   //print_tree(t23_out);
- 
+
+  printf("\n");
   return 0;
 
 }
